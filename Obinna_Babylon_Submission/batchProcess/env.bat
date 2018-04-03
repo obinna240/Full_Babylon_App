64597c149b07c3c982fa39b634ef75ddf538fe4e
obinna240 <obinna240@yahoo.co.uk>
2018-04-03 09:05:40 +0100

    FULL HEALTHCARE NLP APP WITH GATE RESOURCES

---
+++ Obinna_Babylon_Submission/batchProcess/env.bat
@@ -0,0 +1,4 @@
+
+SET BABYLONDIR=C:\Babylon\batcher
+SET JDK_PATH="C:\Program Files\Java\jdk"
+SET CLASSPATH=%BABYLONDIR%\classes;%BABYLONDIR%
---
+++ batchProcess/run.bat
@@ -0,0 +1,2 @@
+call env.bat
+%JDK_PATH%\bin\java -Xmx2448M -cp %CLASSPATH% com.util.DocProcessor http://www.nhs.uk/Conditions/Obsessive-compulsive-disorder/Pages/Symptoms.aspx obsessive-compulsive-disorder.txt 
